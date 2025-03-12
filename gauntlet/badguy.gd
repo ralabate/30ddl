b38b197619cce8d1f6ceeaed1bc3b3c986f93896
Carlos Quinones <cquinones.private@gmail.com>
2025-03-12 10:42:59 -0400

    first pass at navigation

--- gauntlet/badguy.gd
+++ gauntlet/badguy.gd
@@ -1,15 +1,42 @@
 extends CharacterBody3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+@onready var navigation_agent: NavigationAgent3D = %NavigationAgent3D
+
+var movement_speed: float = 2.0
+
+
+func _ready():
 	add_to_group("badguys")
 
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.target_desired_distance = 0.5
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		#print("finished!")
+		return
+
+	var current_agent_position: Vector3 = global_position
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	
+	#print(
+		#"velocity: ", velocity, 
+		#" current: ", current_agent_position, 
+		#" next: ", next_path_position,
+		#" target: ", navigation_agent.target_position)
+	
+	move_and_slide()
 
 
 func damage():
 	queue_free()
+
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
--- gauntlet/main.gd
+++ gauntlet/main.gd
@@ -2,20 +2,27 @@ extends Node3D
 
 
 @onready var player = %Player
+@onready var nav_timer = %BadguyNavTimer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.shoot.connect(_on_player_shoot)
+	
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	nav_timer.start()
 
 func _on_player_shoot(bullet_template: PackedScene, rotation: Vector3, location: Vector3):
 	var bullet = bullet_template.instantiate()
 	add_child(bullet)
 	bullet.rotation = rotation
 	bullet.position = location
+
+
+func _on_badguy_nav_timer_timeout() -> void:
+	print("nav timer!")
+	var badguys = get_tree().get_nodes_in_group("badguys")
+	for badguy in badguys:
+		badguy.set_movement_target(player.global_position)
