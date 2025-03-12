18f59bdd0bdb40ac35791c75162143cab4790417
Carlos Quinones <cquinones.private@gmail.com>
2025-03-12 08:48:25 -0400

    baddies

--- gauntlet/bullet.gd
+++ gauntlet/bullet.gd
@@ -4,9 +4,16 @@ extends Area3D
 @export var SPEED = 10
 
 
+func _ready() -> void:
+	add_to_group("player_bullets")
+
+
 func _physics_process(delta: float) -> void:
 	translate_object_local(Vector3.FORWARD * SPEED * delta)
 
 
 func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("badguys"):
+		body.damage()
+
 	queue_free()
